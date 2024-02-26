ec8d023f867681891c051d3e8f2198817eec3b29
Adolph Makhado <adolphmakhado4@gmail.com>
2024-02-26 08:39:36 +0200

    made changes to my header to keep it ontop

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
-let section = document.querySelectorAll('section')
-let navLink = document.querySelectorAll('header nav a')
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
 
 window.onscroll = () => {
-    section.forEach(sec => {
+    sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop-150;
+        let offset = sec.offsetTop;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute('class');
+        let id = sec.getAttribute('id');
 
-        if(top > offset && top < offset + height){
-            navLink.forEach(links => {
-                links.classList.remove('active')
-                document.querySelector('header nav a[href*='+id+']').classList.add('active')
-            })
-        }
-    })
-}
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            });
+        };
+    });
+};
